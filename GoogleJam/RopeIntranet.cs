e31cee2fbcfd1414dea3f022f1590d0d95b35714
Felipe Vaz <felipevnunes@gmail.com>
2017-01-23 18:13:35 -0600

    RopeIntranet and StoreCredit

---
+++ GoogleJam/RopeIntranet.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoogleJam
+{
+    class RopeIntranet
+    {
+        public RopeIntranet(string filePath)
+        {
+            var strings = File.ReadLines(filePath).ToList();
+            var enumerator = strings.GetEnumerator();
+            enumerator.MoveNext();
+            int testsNumber = Convert.ToInt32(enumerator.Current);
+            enumerator.MoveNext();
+            int testCase = 1;
+            var finalResult = new List<string>();
+
+            while (testsNumber > 0)
+            {
+                int wiresNumber = Convert.ToInt32(enumerator.Current);
+                enumerator.MoveNext();
+                var wires = new List<Tuple<int, int>>();
+                while (wiresNumber > 0)
+                {
+                    var wireString = enumerator.Current.Split(' ');
+                    wires.Add(new Tuple<int, int>(Convert.ToInt32(wireString[0]), Convert.ToInt32(wireString[1])));
+                    enumerator.MoveNext();
+                    wiresNumber--;
+                }
+
+                int crosses = 0;
+                for (var i = 0; i < wires.Count; i++)
+                {
+                    for (var i2 = i + 1; i2 < wires.Count; i2++)
+                    {
+                        if(wiresCross(wires[i], wires[i2]))
+                        {
+                            crosses++;
+                        }
+                    }
+                }
+                finalResult.Add("Case #" + testCase++ + ": " + crosses);
+                testsNumber--;
+            }
+
+            File.WriteAllLines("result.out", finalResult);
+        }
+
+        public bool wiresCross(Tuple<int, int> wire1, Tuple<int, int> wire2)
+        {
+            return (wire1.Item1 < wire2.Item1 && wire1.Item2 > wire2.Item2)
+                || (wire1.Item1 > wire2.Item1 && wire1.Item2 < wire2.Item2);
+        }
+    }
+}
---
+++ GoogleJam/StoreCreditProblem.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace GoogleJam
+{
+    class StoreCreditProblem
+    {
+        public StoreCreditProblem(string filePath)
+        {
+            var strings = File.ReadLines(filePath).ToList();
+            var finalResult = new List<string>();
+            var samplesNumber = Convert.ToInt32(strings[0]);
+            var iterator = 1;
+            var current = 1;
+            while (current <= samplesNumber)
+            {
+                var credit = Convert.ToInt32(strings[iterator++]);
+                var itemsNumber = strings[iterator++];
+                var items = strings[iterator++].Split(' ').ToList().ConvertAll(i => Convert.ToInt32(i));
+                int bestMatch = credit;
+                var result = string.Empty;
+
+                for (var i = 0; i < items.Count && bestMatch != 0; i++)
+                {
+                    var creditTemp = credit;
+                    creditTemp -= items[i];
+                    for (var i2 = 0; i2 < items.Count && bestMatch != 0; i2++)
+                    {
+                        if (i2 == i) {
+                            continue;
+                        }
+                        var creditTemp2 = creditTemp - items[i2];
+                        if(creditTemp2 >= 0 && creditTemp2 < bestMatch)
+                        {
+                            bestMatch = creditTemp2;
+                            result = (i + 1) + " " + (i2 + 1);
+                        }
+                    }
+                }
+                
+                finalResult.Add("Case #" + current + ": " + result);
+
+                current++;
+            }
+
+            File.WriteAllLines("result.out", finalResult);
+        }
+    }
+}
